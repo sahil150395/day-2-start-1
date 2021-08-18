f14fda0f33d0ad516b489666a3d23df2af14155c
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-18 06:01:11 +0000

    demo class

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
 #Data Types
+
+#String
+#prints the first character of a string
+print("Hello"[4])
+
+#Integer
+print(123_345)
+
+#Float 
+print(3.14159)
+
+#Boolean
+#True
+#False
+
+#Type checking
+print(type)
+
+#Type casting
+# num_char = len(input("What is your name? "))
+# new_num_char = str(num_char)
+# print("Your name has "+ new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+
+print(3*(3+3)/3-3)
+
+#number manipulation
+print(round(2.6666666))
+print(round(2.66666666,2))
+
+score = 0
+print(f"Your score is {score}")
